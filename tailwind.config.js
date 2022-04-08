3b93ecdfd8c2498824fb44a7e2eeb4abf6d05e7c
Manh. Nguyen Duc - CMC Global DU3.1 <ndmanh2@cmcglobal.vn>
2022-04-08 17:48:19 +0700

    feat(mobile): add styles for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,6 +15,17 @@ module.exports = {
                 navbar: {
                     dark: '#20202380'
                 }
+            },
+            screens: {
+                'mobile': '320px',
+                
+                'tablet': '640px',
+                // => @media (min-width: 640px) { ... }
+
+                'laptop': '1024px',
+                // => @media (min-width: 1024px) { ... }
+
+                'desktop': '1280px',
             }
         },
 
